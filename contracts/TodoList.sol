45089e546878f02790fbbe6bd0caa39dbe094e70
Jiarui Cao <jerrycao030111@outlook.com>
2022-12-28 20:10:32 -0600

    Setup tasks

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -3,4 +3,21 @@ pragma solidity >=0.5.0;
 
 contract TodoList {
     uint public task_count = 0;
+
+    struct Task {
+        uint id;
+        string content;
+        bool completed;
+    }
+
+    mapping(uint => Task) public tasks;  
+
+    constructor() public {
+        create_task("Build a Web 3.0 Piazza");
+    }
+
+    function create_task(string memory _content) public {
+        task_count += 1;
+        tasks[task_count] = Task(task_count, _content, false);
+    }
 }
